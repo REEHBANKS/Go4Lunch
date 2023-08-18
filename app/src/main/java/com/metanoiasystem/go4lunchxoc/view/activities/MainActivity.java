5aa9dbe32ce09d270fded2fad565e60e7a73d0c6
imac27 <r.sainfort@gmail.com>
2023-08-18 15:26:29 +0200

    Defined project architecture with MVVM and Clean Architecture principles
    - Created packages for model, view, and viewmodels for MVVM
    - Organized code according to Clean Architecture with data, domain, and network packages
    - Set up use cases and repository classes for managing business logic and data access

--- app/src/main/java/com/metanoiasystem/go4lunchxoc/view/activities/MainActivity.java
+++ app/src/main/java/com/metanoiasystem/go4lunchxoc/view/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.metanoiasystem.go4lunchxoc;
+package com.metanoiasystem.go4lunchxoc.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.metanoiasystem.go4lunchxoc.R;
 
 public class MainActivity extends AppCompatActivity {
 
