53a44a4220ad5bee331ca74d2815e4bbb7627122
imac27 <r.sainfort@gmail.com>
2023-09-03 17:12:24 +0200

    Implemented onActivityResult handling for Firebase Authentication with Snackbar feedback and navigation to MainActivity

--- app/src/main/java/com/metanoiasystem/go4lunchxoc/view/activities/ConnexionActivity.java
+++ app/src/main/java/com/metanoiasystem/go4lunchxoc/view/activities/ConnexionActivity.java
@@ -2,9 +2,13 @@ package com.metanoiasystem.go4lunchxoc.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.util.Logger;
 import com.metanoiasystem.go4lunchxoc.R;
@@ -77,6 +81,51 @@ public class ConnexionActivity extends AppCompatActivity {
                         .setLogo(R.drawable.ic_login_24)
                         .build(),
                 RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // Show a Snackbar with a message
+    private void showSnackBar(String message) {
+        Snackbar.make(binding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+
+                showSnackBar(getString(R.string.connection_succeed));
+                startMainActivity();
+
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError() != null) {
+                    if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+                }
+            }
+        }
+    }
+
+    // Launching Main Activity
+    private void startMainActivity() {
+        // Create an Intent object for the MainActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        // Start the MainActivity
+        startActivity(intent);
     }
 
 }
